21413f4fe52e48b685524ea7280b8b8f2ba67c04
nguyenhuyquang23091 <nguyenhuyquang230904@gmail.com>
2025-06-01 14:46:46 +0700

    neo4j

--- src/main/java/com/profile/profile_service/controller/UserProfileController.java
+++ src/main/java/com/profile/profile_service/controller/UserProfileController.java
@@ -3,12 +3,15 @@ package com.profile.profile_service.controller;
 
 import com.profile.profile_service.dto.request.ProfileCreationRequest;
 import com.profile.profile_service.dto.response.ProfileCreationResponse;
+import com.profile.profile_service.entity.UserProfile;
 import com.profile.profile_service.service.UserProfileService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,4 +27,9 @@ public class UserProfileController {
     public ProfileCreationResponse getProfile( @PathVariable  String profileId){
         return userProfileService.getOneUserProfile(profileId);
     }
+    @GetMapping()
+    public List<UserProfile> getProfile(){
+        return userProfileService.getAllUserProfile();
+    }
+
 }
